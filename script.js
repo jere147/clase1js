5067eb8700ee4ae97fce9273d9a91bf7bd30db21
jeremias <jere_a@hotmail.com>
2021-11-02 21:37:59 -0300

    clase 1 JS

---
+++ script.js
@@ -0,0 +1,11 @@
+let  nombre  =  prompt('Ingrese su nombre');
+let  suNombre  =   " "  +  "Hola Bienvenido en que podemos ayudarte"  +  " "  +  nombre;
+alert ( suNombre );
+let  mensaje  =  "bienvenido a Nuestro Sitio";
+console.log ( mensaje  + " "  +  nombre  );
+
+let  numero  =  parseInt (prompt('Ingrese un numero'));
+let  nume1  =  parseInt (35)
+let  suma  =   numero  +  nume1 ;
+console.log (suma);
+alert (suma);
