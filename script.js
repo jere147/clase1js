09c91848435b58809b1f6467fb98e8d4830c0a03
jeremias <jere_a@hotmail.com>
2021-11-04 20:53:44 -0300

    clase 2

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let  nombreUsuario = prompt  ("Introduzca su nombre");
+let  apellidoUsuario  =  prompt  ("Introduzca su apellido");
+let  edadUsuario  =  prompt ("Introduzca su edad");
+let  domicilioUsuario  =  prompt ("Introduzca su Domicilio");
+if  ((nombreUsuario  ===  "" )  ||  ( apellidoUsuario  ===  "" )  ||  (edadUsuario  ===  "" ) || (domicilioUsuario  ===  "" ) ) {
+    consola.log  ( "No ingresaste los valores requeridos" );
+}    
+
